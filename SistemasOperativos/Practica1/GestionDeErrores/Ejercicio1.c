aa50ffdc4e6d60397617c2111bedec2ccfe6108d
Diego <alrp.diego@gmail.com>
2021-10-25 13:27:59 +0200

    Modificaciones Gestion de Errores

--- SistemasOperativos/Practica1/GestionDeErrores/Ejercicio1.c
+++ SistemasOperativos/Practica1/GestionDeErrores/Ejercicio1.c
@@ -3,9 +3,12 @@
 #include <unistd.h>
 
 int main(){
+
   char *s;
+
   if (setuid(0) == -1){
 	   perror(s);
 	}
+  
   return 1;
 }
--- SistemasOperativos/Practica1/GestionDeErrores/Ejercicio3.c
+++ SistemasOperativos/Practica1/GestionDeErrores/Ejercicio3.c
@@ -6,10 +6,13 @@
 const int MAX_ERR=255;
 
 int main(){
+
   char *s;
+
   for (int i = 0; i < MAX_ERR; i++){
-	   //perror(s);
+
      printf("ERROR(%d): %s\n", i, strerror(i));
 	}
+  
   return 1;
 }
