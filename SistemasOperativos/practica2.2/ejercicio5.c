7779f2bcd2f04b8cb3a212c87179f2ff514eea05
Diego <alrp.diego@gmail.com>
2021-11-06 13:21:14 +0100

    Ejercicio 5

---
+++ SistemasOperativos/practica2.2/ejercicio5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv) {
+
+	char pathname[] = "/home/cursoredes/Documents/Practica2/ejercicio5.txt";
+
+	int fd = open(pathname, O_CREAT | O_TRUNC | O_RDWR, 644);
+	
+	if (fd == -1) {
+		
+		printf("ERROR(%d): %s\n", errno, strerror(errno));
+	}
+	else {
+		
+		printf("FileDescriptor: %d\n", fd);
+	}
+}
