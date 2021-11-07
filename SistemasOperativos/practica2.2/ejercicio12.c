c9098bcf691e07dcb75ab92a6d57f0f90c97745e
Diego <alrp.diego@gmail.com>
2021-11-07 18:14:52 +0100

    Ejercicio 12

---
+++ SistemasOperativos/practica2.2/ejercicio12.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv) {
+
+	char* path = argv[1];
+
+	int fd = open(path, O_CREAT | O_TRUNC | O_RDWR, 0777);
+	
+	if (fd == -1) {
+		
+		printf("ERROR(%d): %s\n", errno, strerror(errno));
+	}
+	else {
+		
+		int d = dup2(fd, 1);
+
+		if (d == -1) printf("ERROR(%d): %s\n", errno, strerror(errno));
+		else {
+			
+			printf("Prueba!\n");
+			printf("Esta es una cadena de prueba\n");
+			printf("1, 2, 3,... probando");
+		}
+	}
+}
