7c0b8dd04ac6a15d9930bc6448e5590609f6c78f
Diego <alrp.diego@gmail.com>
2021-11-22 09:49:45 +0100

    Arreglado comentario Ejercicio 2

--- SistemasOperativos/practica2.3/ejercicio2.c
+++ SistemasOperativos/practica2.3/ejercicio2.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv) {
 
 	printf("pid: %d\n", pid);
 
-	//Obtener la politica de planificacion
+	//Obtener la política de planificación
 	//Se puede poner como PID el 0, debido a que eso indica que es el
 	//PID del propio proceso
 	int policy = sched_getscheduler(pid);
@@ -19,7 +19,7 @@ int main(int argc, char **argv) {
 	// 1: SCHED_FIFO
 	// 2: SCHED_RR
 
-	printf("Plotica numero: %d\n", policy);
+	printf("Política número: %d\n", policy);
 
 	if (policy == -1) {
 
@@ -27,16 +27,16 @@ int main(int argc, char **argv) {
 	}
 	else if (policy == SCHED_OTHER) {
 
-		printf("Politica: %s\n", "SCHED_OTHER");
+		printf("Política: %s\n", "SCHED_OTHER");
 		
 	}
 	else if (policy == SCHED_FIFO) {
 
-		printf("Politica: %s\n", "SCHED_FIFO");
+		printf("Política: %s\n", "SCHED_FIFO");
 	}
 	else if (policy == SCHED_RR) {
 
-		printf("Politica: %s\n", "SCHED_RR");
+		printf("Política: %s\n", "SCHED_RR");
 	}
 
 	//Obtener prioridad de planificacion
@@ -57,11 +57,11 @@ int main(int argc, char **argv) {
 		//p.sched_priority devuelve la prioridad actual
 		printf("Prioridad: %d\n", p.sched_priority);
 
-		//De parametro se pasa la politica de prioridad
+		//Como parámetro se pasa la política de prioridad
 		int max_priority = sched_get_priority_max(policy);
 		int min_priority = sched_get_priority_min(policy);
 
-		printf("Maxima Prioridad: %d\n", max_priority);
-		printf("Minima Prioridad: %d\n", min_priority);
+		printf("Máxima Prioridad: %d\n", max_priority);
+		printf("Mínima Prioridad: %d\n", min_priority);
 	}
 }
