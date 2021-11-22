2e7cd4e2165950d0aba6810146505f488d585e5d
Diego <alrp.diego@gmail.com>
2021-11-22 09:50:00 +0100

    Mejorado el Ejercicio 3

--- SistemasOperativos/practica2.3/ejercicio3.sh
+++ SistemasOperativos/practica2.3/ejercicio3.sh
@@ -4,3 +4,11 @@ sudo chrt -f -p 12 17179
 
 #La prioridad en el programa es de 12
 #Si se heredan los atributos de planificacion
+
+# También se puede escribir de la siguente manera
+# que está mejor hecho
+
+sudo chrt -f -p 12 $$
+# $$ es el proceso actual
+
+./ejercicio2
