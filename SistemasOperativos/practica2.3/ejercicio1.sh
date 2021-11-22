5146f343f4dd71d5447920cd4062df98dcd1b06e
Diego <alrp.diego@gmail.com>
2021-11-22 09:26:49 +0100

    Ejercicio 1 chrt

--- SistemasOperativos/practica2.3/ejercicio1.sh
+++ SistemasOperativos/practica2.3/ejercicio1.sh
@@ -1,3 +1,12 @@
-sudo renice -n -10 -p $$ #el $$ es el proceso actual
+sudo renice -n -10 -p $$ 
+#el $$ es el proceso actual
+#-p es la prioridad
 
 nice
+
+sudo chrt -f -p 12 $$
+# recordar que el $$ es para el proceso actual
+# -f es para indicar que es FIFO
+
+chrt -p $$
+# para preguntar la prioridad actual
