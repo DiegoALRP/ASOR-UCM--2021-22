3b73bb9cce79ae73e957bfda98be86259358d432
Diego <alrp.diego@gmail.com>
2021-11-22 09:37:54 +0100

    Agregado comentarios ejercicio2

--- SistemasOperativos/practica2.3/ejercicio2.c
+++ SistemasOperativos/practica2.3/ejercicio2.c
@@ -10,8 +10,11 @@ int main(int argc, char **argv) {
 	printf("pid: %d\n", pid);
 
 	//Obtener la politica de planificacion
+	//Se puede poner como PID el 0, debido a que eso indica que es el
+	//PID del propio proceso
 	int policy = sched_getscheduler(pid);
 
+	// Politicas:
 	// 0: SCHED_OTHER
 	// 1: SCHED_FIFO
 	// 2: SCHED_RR
@@ -38,8 +41,11 @@ int main(int argc, char **argv) {
 
 	//Obtener prioridad de planificacion
 
+	//Estructura que se pasa como parámetro
+	//Almacena en la estructura la información de la prioridad
 	struct sched_param p;
-
+	
+	//Para obtener información de la prioridad actual
 	int priority = sched_getparam(pid, &p);
 
 	if (priority == -1) {
@@ -48,8 +54,10 @@ int main(int argc, char **argv) {
 	}
 	else {
 
+		//p.sched_priority devuelve la prioridad actual
 		printf("Prioridad: %d\n", p.sched_priority);
 
+		//De parametro se pasa la politica de prioridad
 		int max_priority = sched_get_priority_max(policy);
 		int min_priority = sched_get_priority_min(policy);
 
