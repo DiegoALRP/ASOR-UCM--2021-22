bba18545d5a42b7ad25b6e71b917d24cc3409aee
Diego <alrp.diego@gmail.com>
2021-11-26 19:48:59 +0100

    Ejercicio 9

---
+++ SistemasOperativos/practica2.3/ejercicio9.sh
@@ -0,0 +1,7 @@
+man kill
+man pkill
+
+kill [pid] # Envía la señal de KILL a un proceso PID.
+kill -l # Lista todas las señales.
+kill -9 -1 # Mata a todos los procesos que puede matar
+kill [pid] -s [signal] # Envia la señal especificada
