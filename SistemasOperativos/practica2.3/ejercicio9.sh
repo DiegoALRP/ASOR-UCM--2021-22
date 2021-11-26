1c78b10e341dde539632847b082a0167dd1990d6
Diego <alrp.diego@gmail.com>
2021-11-26 19:52:27 +0100

    Ejercicio 9

--- SistemasOperativos/practica2.3/ejercicio9.sh
+++ SistemasOperativos/practica2.3/ejercicio9.sh
@@ -5,3 +5,8 @@ kill [pid] # Envía la señal de KILL a un proceso PID.
 kill -l # Lista todas las señales.
 kill -9 -1 # Mata a todos los procesos que puede matar
 kill [pid] -s [signal] # Envia la señal especificada
+
+# Ejemplos
+kill -SIGSTOP 2652  # Ctrl + Z
+kill -SIGCONT 2652  # foreground
+kill -SIGQUIT 2652  # Ctrl + C (quit)
