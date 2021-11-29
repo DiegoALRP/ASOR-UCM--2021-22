ee8ebc5d6e9d284e925eb55f7a20126695a5f5de
Diego <alrp.diego@gmail.com>
2021-11-29 09:44:53 +0100

    Intento 1 Ejercicio 1

---
+++ SistemasOperativos/practica2.4/ejercicio1.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+
+int main() {
+    
+    int tuberia[2];
+    if (pipe(tuberia) == -1) {
+
+        perror("pipe");
+        return -1;
+    }
+    int pid = fork();
+
+    switch(pid) {
+
+        case -1:
+            perror("fork");
+            return -1;
+            break;
+        case 0:
+            printf("Hijo, lector de la tuberia (%i)\n", pid);
+            close(1);
+            dup2(tuberia[1], 1);
+            close(tuberia[0]);
+            close(tuberia[1]);
+            execlp(argv[3], argv[3], argv[4], 0);
+
+        case 1:
+            printf("Padre, escritor de la tuberia (%i)\n", pid);
+            close(1);
+            dup2(tuberia[1], 1);
+            close(tuberia[0]);
+            close(tuberia[1]);
+            execlp(argv[1], argv[1], argv[2], 0);
+    }
+}
