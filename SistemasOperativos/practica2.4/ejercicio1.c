ae824d1d490f3c83db0c440e74780e9feff511fe
Diego <alrp.diego@gmail.com>
2021-11-29 10:20:35 +0100

    Revisar Ejercicio 1

--- SistemasOperativos/practica2.4/ejercicio1.c
+++ SistemasOperativos/practica2.4/ejercicio1.c
@@ -3,36 +3,45 @@
 #include <errno.h>
 #include <unistd.h>
 
-int main() {
-    
-    int tuberia[2];
+int main(int argc, char** argv) {
+
+	int tuberia[2];
+
     if (pipe(tuberia) == -1) {
 
         perror("pipe");
         return -1;
     }
+
     int pid = fork();
 
-    switch(pid) {
+	switch(pid) {
 
-        case -1:
+        case -1: //Error fork
             perror("fork");
             return -1;
             break;
-        case 0:
+
+		case 0:	//Proceso Hijo
+
             printf("Hijo, lector de la tuberia (%i)\n", pid);
+
             close(1);
             dup2(tuberia[1], 1);
             close(tuberia[0]);
             close(tuberia[1]);
+
             execlp(argv[3], argv[3], argv[4], 0);
 
-        case 1:
+		case 1:	//Proceso Padre
+
             printf("Padre, escritor de la tuberia (%i)\n", pid);
+
             close(1);
             dup2(tuberia[1], 1);
             close(tuberia[0]);
             close(tuberia[1]);
+
             execlp(argv[1], argv[1], argv[2], 0);
     }
 }
