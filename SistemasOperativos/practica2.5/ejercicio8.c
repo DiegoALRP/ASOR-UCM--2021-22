36e7eb90d2013d4cc3477211be74dd0468eb5f56
Diego <alrp.diego@gmail.com>
2021-12-19 11:32:21 +0100

    Copiado Ejercicio 6

--- SistemasOperativos/practica2.5/ejercicio8.c
+++ SistemasOperativos/practica2.5/ejercicio8.c
@@ -9,25 +9,33 @@
 #include <string.h>
 //time
 #include <time.h>
-//read
+//Select
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/types.h>
 #include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+//Listen & bind
+#include <sys/types.h>
+#include <sys/socket.h>
 
 
 int main(int argc, char** argv) {
 
 	if (argc != 3) {
 
-		printf("Introduce los parámetros necesarios: %s <dirección> <puerto>\n", 
-					argv[0]);
+		printf("Introduce los parámetros necesarios: %s <dirección> <puerto>\n", argv[0]);
 		return -1;
 	}
-
+	
 	printf("argv[1]: %s\nargv[2]: %s\n", argv[1], argv[2]);
 	
 	//Gestión de Direcciones
 	struct addrinfo hints;
 	struct addrinfo *resultInfo;
 
+	//Copiado del man
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
 	hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
@@ -36,78 +44,104 @@ int main(int argc, char** argv) {
 	hints.ai_canonname = NULL;
 	hints.ai_addr = NULL;
 	hints.ai_next = NULL;
-
+	
 	int rc = getaddrinfo(
-					argv[1],
-					argv[2],
-					&hints,
-					&resultInfo);
-
+						argv[1],
+						argv[2],
+						&hints,
+						&resultInfo);
+						
 	if (rc != 0) {
-
-		fprintf(stderr, "getaddrinfo: %s \n", gai_strerror(rc));
+	
+		fprintf(stderr, "Error getaddrinfo(): %s \n", gai_strerror(rc));
 		return -1;
 	}
-
+	
 	int socketTCP = socket(resultInfo->ai_family, resultInfo->ai_socktype, 0);
-
+	
 	if (socketTCP == -1) {
-
-		fprintf(stderr, "Error socket()\n");
+	
+		perror("Error socket()\n");
 		return -1;
 	}
-
 	
-	rc = connect(socketTCP, resultInfo->ai_addr, resultInfo->ai_addrlen);
-
-	if (rc == -1) {
-
-		fprintf(stderr, "Error connect()\n");
+	int b = bind(socketTCP, (struct sockaddr *) resultInfo->ai_addr, resultInfo->ai_addrlen);
+	
+	if (b == -1) {
+	
+		perror("Error bind()\n");
 		return -1;
 	}
-
-	freeaddrinfo(resultInfo);
 	
-	char buf[80];
-	memset(buf,0,sizeof(buf));
-
+	//El segundo parámetro tiene que ser potencia de 2, y como mínimo 16
+	int l = listen(socketTCP, 16);
+	
+	if (l == -1) {
+	
+		fprintf(stderr, "Error listen()\n");
+		return -1;
+	}
+	
 	while (1) {
-
-		ssize_t bytes = read(0, buf, 80);
-		if (bytes == -1) {
+	
+		struct sockaddr_storage addr;
+		socklen_t addrlen = sizeof(addr);
 		
-			fprintf(stderr, "Error read()\n");
+		int acc = accept(socketTCP, (struct sockaddr *) &addr, &addrlen);
+		
+		if (acc == -1) {
+		
+			fprintf(stderr, "Error accept()\n");
 			return -1;
 		}
 		
-		buf[bytes] = '\0';
+		char host[NI_MAXHOST];
+		char serv[NI_MAXSERV];
 		
-		if (strcmp(buf, "Q\n") == 0) {
+		int nameInfo = getnameinfo((struct sockaddr *) &addr, addrlen,
+									host, NI_MAXHOST,
+									serv, NI_MAXSERV,
+									NI_NUMERICHOST|NI_NUMERICSERV);
+									
+		if (nameInfo == -1) {
+		
+			fprintf(stderr, "Error getnammeinfo()\n");
+		}
+		
+		printf("Conexión desde Host: %s Puerto: %s\n", host, serv);
+		
+		//Comprobar mensaje!!
+		char buf[80];
+		ssize_t bytes;
+		
+		//OJO: el recv se hace con el socket del accept, no el socket del socket()
+		while (bytes = recv(acc, buf, 80, 0)) {
+		
+			if (bytes == -1) {
 			
-			printf("Saliendo...\n");
-			break;
+				fprintf(stderr, "Error recv()\n");
+			}
+			
+			buf[bytes] = '\0';
+			printf("\tMensaje: %s\n", buf);
+			send(acc, buf, bytes, 0);
 		}
 		
-		rc = send(socketTCP, buf, bytes, 0);
-		if (rc == -1) {
+		printf("Conexión terminada\n");
 		
-			fprintf(stderr, "Error send()\n");
-			return -1;
-		}
-		
-		bytes = recv(socketTCP, buf, 80, 0);
-		if (bytes == -1) {
-		
-			fprintf(stderr, "Error recv()\n");
-			return -1;
-		}
-		
-		buf[bytes] = '\0';
-		printf("%s", buf);
-	}	
-
-	//freeaddrinfo(resultInfo);
+		close(acc);
+	}
+	
 	close(socketTCP);
+	
+	freeaddrinfo(resultInfo);
 
 	return 0;
 }
+
+
+
+
+
+
+
