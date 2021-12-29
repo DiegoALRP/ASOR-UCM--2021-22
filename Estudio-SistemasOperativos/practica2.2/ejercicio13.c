74a0fa7ec6744e74b77a7eb73b3315efe86e2389
Diego <alrp.diego@gmail.com>
2021-12-29 14:32:00 +0100

    Ejercicio 13

---
+++ Estudio-SistemasOperativos/practica2.2/ejercicio13.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+//open
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+       
+int main(int argc, char *argv[]) {
+
+	if (argc != 2) {
+	
+		printf("Introduce la ruta del fichero\n");
+		printf("Usage: %s <filename>\n", argv[0]);
+		
+		return -1;
+	}
+
+	printf("argv[0]: %s, argv[1]: %s\n", argv[0], argv[1]);
+	
+	int oldfd = 1;
+	int outfd = 2;
+	int newfd = open(argv[1], O_CREAT | O_TRUNC | O_RDWR, 0777);
+	
+	if (newfd == -1) {
+	
+		printf("Error open() %d: %s\n", errno, strerror(errno));
+		
+		return -1;
+	}
+	
+	if (dup2(newfd, oldfd) == -1) {
+	
+		printf("Error dup2 %d: %s\n", errno, strerror(errno));
+		
+		return -1;
+	}
+	
+	if (dup2(newfd, outfd) == -1) {
+	
+		printf("Error dup2 %d: %s\n", errno, strerror(errno));
+		
+		return -1;
+	}
+	
+	printf("Hola\n");
+	printf("Esto es una prueba\n");
+	
+	perror("Esto es un mensaje de error\n");
+	
+	close(newfd);
+	
+	return 0;
+}
