a1f707f6ad6c245ca4b84072e625483817e06091
Diego <alrp.diego@gmail.com>
2021-12-29 11:33:37 +0100

    Ejercicio 1

---
+++ Estudio-SistemasOperativos/practica2.2/ejercicio1.sh
@@ -0,0 +1,28 @@
+man ls
+
+ls -a
+# all (creo que incluso incluye ficheros no visibles).
+
+ls -l
+# formato de lista extendida.
+
+ls -d
+# Lista los directorios, mas no su contenido.
+
+ls -h
+# Formato para lectura humana.
+
+ls -i
+# imprime el numero de indice (inodo) de cada archivo/fichero.
+
+ls -R
+# Lista los subdirectorios recursivamente.
+
+ls -1
+#Lista cada archivo/fichero por linea.
+
+ls -F
+# Une uno de los indicadores (*/=>@|) a las entradas.
+
+ls --color
+# Colorea la salida
