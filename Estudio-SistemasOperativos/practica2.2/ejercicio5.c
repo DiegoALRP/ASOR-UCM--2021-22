104b52c8044654673c28fb4e84464d9c7bf6846c
Diego <alrp.diego@gmail.com>
2021-12-29 12:32:00 +0100

    Update Ejercicio 5

--- Estudio-SistemasOperativos/practica2.2/ejercicio5.c
+++ Estudio-SistemasOperativos/practica2.2/ejercicio5.c
@@ -1,10 +1,17 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
 
 int main () {
 
-	open("ejercicio5prueba.c", O_CREAT | O_RDWR | O_TRUNC, 0645);
+	int fd = open("ejercicio5prueba.c", O_CREAT | O_RDWR | O_TRUNC, 0645);
+	
+	if (fd == -1) {
+	
+		printf("Error open() %d: %s\n", errno, strerror(errno));
+	}
 	
 	return 0;
 }
