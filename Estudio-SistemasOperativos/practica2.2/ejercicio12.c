57e2bf4deef3eb1e542e9c70728af39985a80c19
Diego <alrp.diego@gmail.com>
2021-12-29 14:51:55 +0100

    Added error control Ejercicio 12

--- Estudio-SistemasOperativos/practica2.2/ejercicio12.c
+++ Estudio-SistemasOperativos/practica2.2/ejercicio12.c
@@ -21,7 +21,13 @@ int main(int argc, char *argv[]) {
 	int oldfd = 1;
 	int newfd = open(argv[1], O_CREAT | O_TRUNC | O_RDWR, 0777);
 	
-
+    if (newfd == -1) {
+	
+		printf("Error open() %d: %s\n", errno, strerror(errno));
+		
+		return -1;
+	}
+    
 	if (dup2(newfd, oldfd) == -1) {
 	
 		printf("Error dup2 %d: %s\n", errno, strerror(errno));
