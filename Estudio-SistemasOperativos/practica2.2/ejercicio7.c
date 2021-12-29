67818932c2c1253fba856fcaafe2ff7c2695d963
Diego <alrp.diego@gmail.com>
2021-12-29 12:38:55 +0100

    Ejercicio 7

---
+++ Estudio-SistemasOperativos/practica2.2/ejercicio7.c
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+
+int main () {
+
+	umask(027);
+
+	int fd = open("ejercicio7prueba.c", O_CREAT | O_RDWR | O_TRUNC, 0645);
+	
+	if (fd == -1) {
+	
+		printf("Error open() %d: %s\n", errno, strerror(errno));
+	}
+	
+	return 0;
+}
