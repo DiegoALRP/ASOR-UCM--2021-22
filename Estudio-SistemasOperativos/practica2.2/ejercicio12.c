76e7b72bed05a92badc384c13cc653e89eed0fdd
Diego <alrp.diego@gmail.com>
2021-12-29 14:32:26 +0100

    Added argument control

--- Estudio-SistemasOperativos/practica2.2/ejercicio12.c
+++ Estudio-SistemasOperativos/practica2.2/ejercicio12.c
@@ -8,11 +8,20 @@
        
 int main(int argc, char *argv[]) {
 
+    if (argc != 2) {
+	
+		printf("Introduce la ruta del fichero\n");
+		printf("Usage: %s <filename>\n", argv[0]);
+		
+		return -1;
+	}
+
 	printf("argv[0]: %s, argv[1]: %s\n", argv[0], argv[1]);
 	
 	int oldfd = 1;
 	int newfd = open(argv[1], O_CREAT | O_TRUNC | O_RDWR, 0777);
 	
+
 	if (dup2(newfd, oldfd) == -1) {
 	
 		printf("Error dup2 %d: %s\n", errno, strerror(errno));
