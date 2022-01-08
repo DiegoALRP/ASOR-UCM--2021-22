58793a575799180bb7885318bdabbb28de782d24
Diego <alrp.diego@gmail.com>
2022-01-08 10:40:40 +0100

    Configuration

---
+++ Estudio-SistemasOperativos/Practica1.5/configuracion.sh
@@ -0,0 +1,83 @@
+#VM1 (Router 1)
+
+[root@localhost ~]# ip link set eth0 up
+[root@localhost ~]# ip a
+1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
+    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
+    inet 127.0.0.1/8 scope host lo
+       valid_lft forever preferred_lft forever
+    inet6 ::1/128 scope host 
+       valid_lft forever preferred_lft forever
+2: eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP group default qlen 1000
+    link/ether 08:00:27:80:97:06 brd ff:ff:ff:ff:ff:ff
+    inet6 fe80::a00:27ff:fe80:9706/64 scope link tentative 
+       valid_lft forever preferred_lft forever
+[root@localhost ~]# ip a add 2001:db8:200:1::1/64 dev eth0
+[root@localhost ~]# ip a
+1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
+    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
+    inet 127.0.0.1/8 scope host lo
+       valid_lft forever preferred_lft forever
+    inet6 ::1/128 scope host 
+       valid_lft forever preferred_lft forever
+2: eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP group default qlen 1000
+    link/ether 08:00:27:80:97:06 brd ff:ff:ff:ff:ff:ff
+    inet6 2001:db8:200:1::1/64 scope global 
+       valid_lft forever preferred_lft forever
+    inet6 fe80::a00:27ff:fe80:9706/64 scope link 
+       valid_lft forever preferred_lft forever
+
+
+#VM2 (Router 2)
+[root@localhost ~]# ip link
+1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN mode DEFAULT group default qlen 1000
+    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
+2: eth0: <BROADCAST,MULTICAST> mtu 1500 qdisc noop state DOWN mode DEFAULT group default qlen 1000
+    link/ether 08:00:27:af:4d:bd brd ff:ff:ff:ff:ff:ff
+3: eth1: <BROADCAST,MULTICAST> mtu 1500 qdisc noop state DOWN mode DEFAULT group default qlen 1000
+    link/ether 08:00:27:f9:34:d8 brd ff:ff:ff:ff:ff:ff
+[root@localhost ~]# ip link set eth0 up
+[root@localhost ~]# ip link set eth1 up
+[root@localhost ~]# ip a add 2001:db8:200:1::2 dev eth0
+[root@localhost ~]# ip a add 2001:db8:200:2::2 dev eth1
+[root@localhost ~]# ip a
+1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
+    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
+    inet 127.0.0.1/8 scope host lo
+       valid_lft forever preferred_lft forever
+    inet6 ::1/128 scope host 
+       valid_lft forever preferred_lft forever
+2: eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP group default qlen 1000
+    link/ether 08:00:27:af:4d:bd brd ff:ff:ff:ff:ff:ff
+    inet6 2001:db8:200:1::2/128 scope global 
+       valid_lft forever preferred_lft forever
+    inet6 fe80::a00:27ff:feaf:4dbd/64 scope link 
+       valid_lft forever preferred_lft forever
+3: eth1: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP group default qlen 1000
+    link/ether 08:00:27:f9:34:d8 brd ff:ff:ff:ff:ff:ff
+    inet6 2001:db8:200:2::2/128 scope global tentative 
+       valid_lft forever preferred_lft forever
+    inet6 fe80::a00:27ff:fef9:34d8/64 scope link 
+       valid_lft forever preferred_lft forever
+
+#VM3 (Router 3)
+[root@localhost ~]# ip link
+1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN mode DEFAULT group default qlen 1000
+    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
+2: eth0: <BROADCAST,MULTICAST> mtu 1500 qdisc noop state DOWN mode DEFAULT group default qlen 1000
+    link/ether 08:00:27:2a:21:17 brd ff:ff:ff:ff:ff:ff
+[root@localhost ~]# ip link set eth0 up
+[root@localhost ~]# ip a add 2001:db8:200:2::3 dev eth0
+[root@localhost ~]# ip a
+1: lo: <LOOPBACK,UP,LOWER_UP> mtu 65536 qdisc noqueue state UNKNOWN group default qlen 1000
+    link/loopback 00:00:00:00:00:00 brd 00:00:00:00:00:00
+    inet 127.0.0.1/8 scope host lo
+       valid_lft forever preferred_lft forever
+    inet6 ::1/128 scope host 
+       valid_lft forever preferred_lft forever
+2: eth0: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc pfifo_fast state UP group default qlen 1000
+    link/ether 08:00:27:2a:21:17 brd ff:ff:ff:ff:ff:ff
+    inet6 2001:db8:200:2::3/128 scope global tentative 
+       valid_lft forever preferred_lft forever
+    inet6 fe80::a00:27ff:fe2a:2117/64 scope link 
+       valid_lft forever preferred_lft forever
